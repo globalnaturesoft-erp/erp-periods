8230852e43ff3726f6238ea8e30c56dbef5aee72
Luan Pham <luanpm88@gmail.com>
2017-12-09 10:26:31 +0700

    period model fix

--- app/models/erp/periods/period.rb
+++ app/models/erp/periods/period.rb
@@ -2,20 +2,20 @@ module Erp::Periods
   class Period < ApplicationRecord
     belongs_to :creator, class_name: 'Erp::User'
     validates :name, :from_date, :to_date, presence: true
-    
+
     # class const
     STATUS_ACTIVE = 'active'
     STATUS_DELETED = 'deleted'
-    
+
     # Filters
     def self.filter(query, params)
       params = params.to_unsafe_hash
-      
+
       # join with users table for search creator
       query = query.joins(:creator)
-      
+
       and_conds = []
-      
+
       #filters
       if params["filters"].present?
         params["filters"].each do |ft|
@@ -26,7 +26,7 @@ module Erp::Periods
           and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
         end
       end
-      
+
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -37,17 +37,17 @@ module Erp::Periods
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
-      
+
       # add conditions to query
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
-      
+
       return query
     end
-    
+
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-      
+
       # order
       if params[:sort_by].present?
         order = params[:sort_by]
@@ -57,13 +57,13 @@ module Erp::Periods
       else
 				query = query.order('created_at desc')
       end
-      
+
       return query
     end
-    
+
     # data for dataselect ajax
     def self.dataselect(keyword='', params='')
-      
+
       query = self.where(status: Erp::Periods::Period::STATUS_ACTIVE)
 
       if keyword.present?
@@ -73,33 +73,33 @@ module Erp::Periods
 
       query = query.order("created_at DESC").limit(20).map{|period| {value: period.id, text: period.name} }
     end
-    
+
     def set_active
       update_columns(status: Erp::Periods::Period::STATUS_ACTIVE)
     end
-    
+
     def set_deleted
       update_columns(status: Erp::Periods::Period::STATUS_DELETED)
     end
-    
+
     def self.set_active_all
       update_all(status: Erp::Periods::Period::STATUS_ACTIVE)
     end
-    
+
     def self.set_deleted_all
       update_all(status: Erp::Periods::Period::STATUS_DELETED)
     end
-    
+
     def is_deleted?
       return status == Erp::Periods::Period::STATUS_DELETED
     end
-    
+
     # Get time array
     def self.get_time_array(params={})
 			times = []
-			
+
 			@today = Date.today
-			
+
 			# global filter
       global_filter = params[:global_filter]
       if global_filter.present?
@@ -108,9 +108,9 @@ module Erp::Periods
 					@today = global_filter[:to_date].to_date
 				end
       end
-      
+
 			number_of_months = 0..@today.month
-			
+
 			number_of_months.to_a.reverse.each do |month_offset|
 				date = @today - month_offset.months
 				if month_offset == @today.month
@@ -125,13 +125,13 @@ module Erp::Periods
 			end
 			return times
 		end
-    
+
     # auto create an year periods
-    def self.create_year_periods(year)      
+    def self.create_year_periods(year)
       date = "#{year}-01-01".to_date.beginning_of_year
-      
+
       return false if !self.where("to_date >= ?", date).empty?
-      
+
       # 12 months
       (0..11).each do |i|
         month = date + i.months
@@ -143,8 +143,8 @@ module Erp::Periods
           status: self::STATUS_ACTIVE,
         )
       end
-      
-      # 4 quaters      
+
+      # 4 quaters
       self.create(
         name: "Quý 1/#{date.year}",
         from_date: date.beginning_of_month,
@@ -173,7 +173,7 @@ module Erp::Periods
         creator_id: Erp::User.first.id,
         status: self::STATUS_ACTIVE,
       )
-      
+
       # half year
       self.create(
         name: "Nửa đầu năm #{date.year}",
@@ -189,7 +189,7 @@ module Erp::Periods
         creator_id: Erp::User.first.id,
         status: self::STATUS_ACTIVE,
       )
-      
+
       # whole year
       self.create(
         name: "Năm #{date.year}",
